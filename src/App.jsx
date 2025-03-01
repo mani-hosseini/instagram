5fba38ed2413d711c39920340c03e0f6fe0192f3
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-03-01 10:11:32 +0330

    build pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {Route, Routes} from "react-router-dom";
+import Signup from "./pages/sign-up/signUp.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" component={App}></Route>
+          <Route  path="/" element={<Signup/>}/>
       </Routes>
     </>
   )
---
+++ src/pages/sign-up/signUp.jsx
@@ -0,0 +1,9 @@
+function Signup() {
+    return (
+        <div>
+            <h1>sign-up</h1>
+        </div>
+    )
+}
+
+export default Signup
