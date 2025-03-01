bbba5b7abca09ac106f22de2bb3d12ae622ef29e
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-03-01 15:05:39 +0330

    build login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Signup from "./pages/sign-up/signUp.jsx";
+import Login from "./pages/login/login.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       <Routes>
           <Route  path="/" element={<Signup />}/>
+          <Route  path="/login" element={<Login/>}/>
       </Routes>
     </>
   )
