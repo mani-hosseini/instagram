339c18666159683d8ac5e9644ea24e91caf0b3ee
Mani hosseini <mani.hosseini.h8@gmail.com>
2025-03-02 10:01:24 +0330

    build nav and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
-import './App.css'
 import {Route, Routes} from "react-router-dom";
-import Signup from "./pages/sign-up/signUp.jsx";
+import Layout from "./pages/nav/nav.jsx";
+import Home from "./pages/home/home.jsx";
+import Search from "./pages/search/search.jsx";
 import Login from "./pages/login/login.jsx";
-import Nav from './pages/nav/nav.jsx';
-import Layout from './pages/nav/nav.jsx';
+import Signup from "./pages/sign-up/signUp.jsx";
+import Profile from "./pages/profile/profile.jsx";
+import Creat from "./pages/create/creat.jsx";
 
 function App() {
-  
-
-  return (
-    <>
-      <Routes>
-      <Route path="/" element={<Layout/>}></Route>
-      </Routes>
-    </>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route index element={<Home/>}/>
+                <Route path="search" element={<Search/>}/>
+                <Route path="create" element={<Creat/>}/>
+                <Route path="profile" element={<Profile/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
-export default App
-
-
-
+export default App;
---
+++ src/pages/create/creat.jsx
@@ -0,0 +1,10 @@
+function Creat() {
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Creat;
